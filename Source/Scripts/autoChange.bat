761c757caec57018fd16c7fda73df72010ced9a0
Dikamsi Young <youngudochi15@gmail.com>
2020-09-15 09:10:44 +0100

    Update autoChange.bat

--- Source/Scripts/autoChange.bat
+++ Source/Scripts/autoChange.bat
@@ -1,9 +1,9 @@
 @echo off
 
 cd ..
-setlocal enableextensions enabledelayedexpansion
 set /a count = 1
 set "list"
+setlocal enableextensions enabledelayedexpansion
 for /f %%a in ('type "set_preferred_time.txt" ^| findstr /R "[0-9]" set_preferred_time.txt') do (
   set /a count += 1
   set "list=!List! %%a"
@@ -17,9 +17,9 @@ for %%I in (%list%) do (
 )
 
 cd Source\Scripts
-For /f "tokens=1-2 delims=/:" %%a in ("%arr[0]%") do (set morning=%%a)
-For /f "tokens=1-2 delims=/:" %%a in ("%arr[1]%") do (set /a evening=%%a)
-For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set /a ctime=%%a)
+For /f "tokens=1-2 delims=/:" %%a in ("%arr[0]%") do (set morning=%%a%%b)
+For /f "tokens=1-2 delims=/:" %%a in ("%arr[1]%") do (set /a evening=%%a%%b)
+For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set /a ctime=%%a%%b)
 
 if /I "%ctime%" GEQ "%morning%" (
    if /I "%ctime%" LSS "%evening%" (
