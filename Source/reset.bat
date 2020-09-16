9d43d1055e360b02da5b63eb52637181882dc514
TheKamsiYoung <47384524+TheKamsiYoung@users.noreply.github.com>
2020-09-16 08:02:31 +0100

    Updated installer

--- Source/reset.bat
+++ Source/reset.bat
@@ -3,6 +3,8 @@
 rem --> Obtain shortened filepath
 for %%A in ("%cd%") do set "filepath=%%~sA"
 
-rem --> Replace autoChange.bat with original version
+rem --> Replace scripts with original version
 cd Scripts\Origin
 type "autoChange.bat" > %filepath%\Scripts\autoChange.bat
+type "changeLight.bat" > %filepath%\Scripts\changeLight.bat
+type "changeDark.bat" > %filepath%\Scripts\changeDark.bat
--- install.bat
+++ install.bat
@@ -1,6 +1,5 @@
 @echo 0ff
 
-
 rem --> Obtain Admin Permission (Code provided by Eneerge sites.google.com/site/eneerge/scripts/batchgotadmin)
 :: BatchGotAdmin
 :-------------------------------------
@@ -60,8 +59,41 @@ rem --> Create Tasks for Light and Dark Theme
 SCHTASKS /CREATE /SC DAILY /TN "Theme Changer\Light Theme" /TR "%filepath%\Source\Scripts\changeLight.bat" /ST %arr[0]% /DU 00:30 /K
 SCHTASKS /CREATE /SC DAILY /TN "Theme Changer\Dark Theme" /TR "%filepath%\Source\Scripts\changeDark.bat" /ST %arr[1]% /DU 00:30 /K
 
-rem --> Setup Change on Logon (Requires Admin)
+rem --> Create Auto Change task
+SCHTASKS /CREATE /TN "Theme Changer\Change at log in" /TR "%filepath%\Source\Scripts\autoChange.bat" /SC ONLOGON /ru %username% /it
+
+rem --> Clear old address in autoChange.bat
 cd Source
-call "setup.bat"
+call "reset.bat"
+
+cd ..
+
+rem --> Copy current address to autoChange.bat because it runs as Admin
+echo rem "Batch is called with Admin rights and needs to be routed back to Source folder" > dir.txt
+echo cd %filepath% >> dir.txt
+type autoChange.bat >> dir.txt
+del /f autoChange.bat
+type dir.txt > autoChange.bat
+del /f dir.txt
+
+rem --> Copy current address to changeLight.bat
+echo rem "Batch is called with Admin rights and needs to be routed back to Source folder" > dir.txt
+echo cd %filepath% >> dir.txt
+type changeLight.bat >> dir.txt
+del /f changeLight.bat
+type dir.txt > changeLight.bat
+del /f dir.txt
+
+rem --> Copy current address to changeDark.bat
+echo rem "Batch is called with Admin rights and needs to be routed back to Source folder" > dir.txt
+echo cd %filepath% >> dir.txt
+type changeDark.bat >> dir.txt
+del /f changeDark.bat
+type dir.txt > changeDark.bat
+del /f dir.txt
+
+echo Auto-dark-mode has been installed successfully!
 
 endlocal
+
+pause
