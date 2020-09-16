50c43aad6ea11d84c13663a54e30f45ee519ace3
TheKamsiYoung <47384524+TheKamsiYoung@users.noreply.github.com>
2020-09-16 07:23:28 +0100

    UPdated install script

--- install.bat
+++ install.bat
@@ -24,8 +24,8 @@ for %%A in ("%cd%") do set "filepath=%%~sA"
 cls
 
 rem --> Create Tasks for Light and Dark Theme
-SCHTASKS /CREATE /SC DAILY /TN "Theme Changer\Light Theme" /TR "%filepath%\Source\changeLight.bat" /ST %arr[0]% /DU 00:30 /K
-SCHTASKS /CREATE /SC DAILY /TN "Theme Changer\Dark Theme" /TR "%filepath%\Source\changeDark.bat" /ST %arr[1]% /DU 00:30 /K
+SCHTASKS /CREATE /SC DAILY /TN "Theme Changer\Light Theme" /TR "%filepath%\Source\Scripts\changeLight.bat" /ST %arr[0]% /DU 00:30 /K
+SCHTASKS /CREATE /SC DAILY /TN "Theme Changer\Dark Theme" /TR "%filepath%\Source\Scripts\changeDark.bat" /ST %arr[1]% /DU 00:30 /K
 
 rem --> Setup Change on Logon (Requires Admin)
 cd Source
