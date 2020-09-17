48ead0525cd1bfd89b09d17f4bb974e2086b7a9a
TheKamsiYoung <47384524+TheKamsiYoung@users.noreply.github.com>
2020-09-17 16:39:45 +0100

    Updated Scripts

--- Source/Scripts/Batch/changeLight.bat
+++ Source/Scripts/Batch/changeLight.bat
@@ -1,5 +1,3 @@
-rem "Batch is called with Admin rights and needs to be routed back to Source folder" 
-cd C:\Users\kboyz\OneDrive\Desktop\Career\DATASC~1\HOMEPR~1\DARKTH~1\AUTO-D~1\Source 
 
 @echo off
 
